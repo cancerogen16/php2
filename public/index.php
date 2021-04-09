fdb22ac59a9178f8864fae49a7cfdb7eecf73c6e
cancerogen-ge <don544@mail.ru>
2021-04-09 17:23:51 +0500

    composer.json

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
 <?php
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require __DIR__ . '/../vendor/autoload.php';
