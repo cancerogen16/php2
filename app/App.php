109da03d7bc480a00b71e41911ff23dd1b2a05c8
cancerogen-ge <don544@mail.ru>
2021-04-13 10:06:26 +0500

    папка app

---
+++ app/App.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app;
+
+/**
+ * Это финальный класс.
+ * От него более нельзя наследоваться.
+ * Class App
+ * @package app
+ */
+final class App {
+    public function __construct() {
+        // Обращаемся к конструктору родительского класса
+        echo 'Создался новый экземпляр класса из папки "app/"<br>';
+    }
+
+    /**
+     * @return string
+     */
+    public function run() {
+        return 'Возвращаем строку, как предписано в родительском классе.';
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion() {
+        return 'version = 0.0.1';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
+
+namespace app;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../autoload.php';
+
+$app = new App;
+
+var_dump($app);
