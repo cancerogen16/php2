843fcd207fc19d2f7e12a26c2ae35c607ee3ddf5
cancerogen-ge <don544@mail.ru>
2021-04-13 10:10:04 +0500

    require_once VENDOR_DIR . 'autoload.php';

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ namespace app;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
 
 $app = new App;
