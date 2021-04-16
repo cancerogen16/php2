f7428e7abf9b678706418669c56fe894b93aef81
cancerogen-ge <don544@mail.ru>
2021-04-16 23:06:59 +0500

    mod_rewrite

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
+$sitePath = realpath(dirname(__FILE__) . DS);
+define('SITE_PATH', $sitePath); // путь к корневой папке сайта
+define('PUBLIC_DIR', SITE_PATH . '/public/');
+define('ADMIN_DIR', SITE_PATH . '/public/admin/');
+define('ENGINE_DIR', SITE_PATH . '/engine/');
+define('VENDOR_DIR', SITE_PATH . '/vendor/');
+define('UPLOADS_DIR', SITE_PATH . '/uploads/');
+define('LAYOUTS_DIR', SITE_PATH . '/layouts/');
+define('TEMPLATES_DIR', SITE_PATH . '/templates/');
+define('BLOCKS_DIR', SITE_PATH . '/blocks/');
+define('IMAGES_DIR', PUBLIC_DIR . '/img/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'geekbrains');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'shop');
+
+require_once VENDOR_DIR . 'autoload.php';
--- config/Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace php2\config;
-
-class Test {
-    public function __construct() {
-        echo "Test constructor";
-    }
-}
