468e5ac73aba4943e35a23993863ae1811d60f0c
cancerogen-ge <don544@mail.ru>
2021-04-17 01:13:07 +0500

    $sitePath = realpath(dirname(__FILE__)) . DS;

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
-$sitePath = realpath(dirname(__FILE__) . DS);
+$sitePath = realpath(dirname(__FILE__)) . DS;
 define('SITE_PATH', $sitePath); // путь к корневой папке сайта
-define('PUBLIC_DIR', SITE_PATH . '/public/');
-define('ADMIN_DIR', SITE_PATH . '/public/admin/');
-define('ENGINE_DIR', SITE_PATH . '/engine/');
-define('VENDOR_DIR', SITE_PATH . '/vendor/');
-define('UPLOADS_DIR', SITE_PATH . '/uploads/');
-define('LAYOUTS_DIR', SITE_PATH . '/layouts/');
-define('TEMPLATES_DIR', SITE_PATH . '/templates/');
-define('BLOCKS_DIR', SITE_PATH . '/blocks/');
-define('IMAGES_DIR', PUBLIC_DIR . '/img/');
+define('PUBLIC_DIR', SITE_PATH . 'public' . DS);
+define('ADMIN_DIR', SITE_PATH . 'public' . DS . 'admin' . DS);
+define('ENGINE_DIR', SITE_PATH . 'engine' . DS);
+define('VENDOR_DIR', SITE_PATH . 'vendor' . DS);
+define('UPLOADS_DIR', SITE_PATH . 'uploads' . DS);
+define('LAYOUTS_DIR', SITE_PATH . 'layouts' . DS);
+define('TEMPLATES_DIR', SITE_PATH . 'templates' . DS);
+define('IMAGES_DIR', PUBLIC_DIR . 'img' . DS);
 
 // DB
 define('DB_DRIVER', 'mysqli');
