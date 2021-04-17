2769b6807ba2cd22b94192eb24aca2eced3a3551
cancerogen-ge <don544@mail.ru>
2021-04-17 12:02:01 +0500

    define('IMAGES_DIR', ASSETS_DIR . 'img' . DS);

--- config.php
+++ config.php
@@ -2,14 +2,13 @@
 define('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
 $sitePath = realpath(dirname(__FILE__)) . DS;
 define('SITE_PATH', $sitePath); // путь к корневой папке сайта
-define('PUBLIC_DIR', SITE_PATH . 'public' . DS);
+define('ASSETS_DIR', SITE_PATH . 'assets' . DS);
 define('ADMIN_DIR', SITE_PATH . 'public' . DS . 'admin' . DS);
 define('ENGINE_DIR', SITE_PATH . 'engine' . DS);
 define('VENDOR_DIR', SITE_PATH . 'vendor' . DS);
 define('UPLOADS_DIR', SITE_PATH . 'uploads' . DS);
-define('LAYOUTS_DIR', SITE_PATH . 'layouts' . DS);
-define('TEMPLATES_DIR', SITE_PATH . 'view' . DS . 'templates' . DS);
-define('IMAGES_DIR', PUBLIC_DIR . 'img' . DS);
+define('TEMPLATES_DIR', SITE_PATH . 'view' . DS);
+define('IMAGES_DIR', ASSETS_DIR . 'img' . DS);
 
 // DB
 define('DB_DRIVER', 'mysqli');
