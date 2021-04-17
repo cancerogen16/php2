bef18fb6e38d252725458cf0a082055e9735b118
cancerogen-ge <don544@mail.ru>
2021-04-17 18:09:23 +0500

    use Engine\Model_Base;

--- model/Model_Product.php
+++ model/Model_Product.php
@@ -2,8 +2,16 @@
 
 namespace Model;
 
-use Engine;
+use Engine\Model_Base;
 
-Class Model_Product Extends Engine\Model_Base {
+Class Model_Product Extends Model_Base {
+    public function __construct($select = false) {
+        parent::__construct($select);
+    }
 
+    public function getProducts($select = []) {
+        $results = $this->getAllRows();
+
+        return $results;
+    }
 }
