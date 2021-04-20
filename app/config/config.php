16fc096c63bc5e6fb5e8b3c7330ea977e01d015c
cancerogen-ge <don544@mail.ru>
2021-04-20 11:55:46 +0500

    define('DIR_VENDOR', SITE_PATH . 'vendor' . DS);

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,7 @@ define('SITE_PATH', $sitePath); // путь к корневой папке са�
 define('DIR_APPLICATION', SITE_PATH . 'app' . DS);
 define('DIR_CONFIG', DIR_APPLICATION . 'config' . DS);
 define('DIR_HELPERS', DIR_APPLICATION . 'helpers' . DS);
-define('DIR_VENDOR', DIR_APPLICATION . 'vendor' . DS);
 define('DIR_TEMPLATES', DIR_APPLICATION . 'views' . DS);
 define('DIR_PUBLIC', SITE_PATH . 'public' . DS);
+define('DIR_VENDOR', SITE_PATH . 'vendor' . DS);
 define('DIR_IMAGE', DIR_PUBLIC . 'img' . DS);
