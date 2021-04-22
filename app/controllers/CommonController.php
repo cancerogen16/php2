b2901ba3a91941c3e4b3461df02cac23ca9f2fd8
cancerogen-ge <don544@mail.ru>
2021-04-22 09:27:02 +0500

    $user_id = (int)$_SESSION["user_id"];

--- app/controllers/CommonController.php
+++ app/controllers/CommonController.php
@@ -11,15 +11,18 @@ class CommonController extends Controller
         $logged = false;
 
         $username = '';
+        $user_id = 0;
 
         if (!empty($_SESSION["username"])) {
             $logged = true;
             $username = htmlspecialchars($_SESSION["username"]);
+            $user_id = (int)$_SESSION["user_id"];
         }
 
         $vars = [
             'logged' => $logged,
             'username' => $username,
+            'user_id' => $user_id,
         ];
 
         $template = 'common/header.html.twig';
