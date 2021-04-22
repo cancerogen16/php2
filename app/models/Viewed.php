07fcd534cd9ef5ccd7632dfce7551120cbb36db8
cancerogen-ge <don544@mail.ru>
2021-04-22 11:33:19 +0500

    return (int)$this->db->one($sql)['views'];

--- app/models/Viewed.php
+++ app/models/Viewed.php
@@ -18,11 +18,11 @@ class Viewed extends Model
      * @param string $url
      * @return mixed
      */
-    public function getViews($user_id = 0, $url = '')
+    public function getViewsCount($user_id = 0, $url = '')
     {
         $sql = "SELECT * FROM viewed WHERE user_id = '" . (int)$user_id . "' AND url = '" . $url . "'";
 
-        return $this->db->one($sql);
+        return (int)$this->db->one($sql)['views'];
     }
 
     /**
@@ -31,7 +31,7 @@ class Viewed extends Model
      * @return int количество просмотров данной страницы данным пользователем
      */
     public function addView($user_id = 0, $url = '') {
-        $views_count = (int)$this->getViews($user_id, $url)['views'];
+        $views_count = $this->getViewsCount($user_id, $url);
 
         if ($views_count == 0) {
             $sql = "INSERT INTO viewed (user_id, url, views) VALUES (:user_id, :url, :views)";
