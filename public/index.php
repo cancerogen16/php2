605b5fda61e0534f227e6aae101815c053a95e61
cancerogen-ge <don544@mail.ru>
2021-04-24 14:17:44 +0500

    public/index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'app/config/config.php';
-require 'app/lib/Dev.php';
+require_once dirname(__DIR__) . '/app/config/config.php';
+require dirname(__DIR__) . '/app/lib/Dev.php';
 
 require_once DIR_VENDOR . 'autoload.php';
 
