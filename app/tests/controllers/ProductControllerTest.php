35a2da597e48fab24ff5d2113e7aac737c8d2d3f
cancerogen-ge <don544@mail.ru>
2021-04-29 16:07:17 +0500

    public function testProductId($product_id, $expected)

--- app/tests/controllers/ProductControllerTest.php
+++ app/tests/controllers/ProductControllerTest.php
@@ -7,13 +7,42 @@ use PHPUnit\Framework\TestCase;
 
 class ProductControllerTest extends TestCase
 {
-    public function testProductAction()
+    private $route;
+    private $product;
+
+    protected function setUp() :void
     {
-        $response = $this->gbTest->dispatchAction('product','GET',['product_id' => 65]);
-        $output = json_decode($response->getOutput(),true);
+        $root = getenv('APP_ROOT');
+        require_once $root . '/app/config/config.php';
+        $routes = require $root . '/app/config/routes.php';
 
-        $this->assertTrue(isset($output['success']) && isset($output['total']));
-        $this->assertRegExp('/HTC Touch HD/', $output['success']);
+        $_POST['test'] = '1';
 
+        $this->route = $routes['product'];
+
+        $this->product = new ProductController($this->route);
+    }
+
+    /**
+     * @dataProvider providerTestProductId
+     * @param $product_id
+     * @param $expected
+     */
+    public function testProductId($product_id, $expected)
+    {
+        $_GET['product_id'] = $product_id;
+
+        $vars = $this->product->productAction();
+
+        $this->assertSame($expected, $vars['product']['product_id']);
+    }
+
+    public function providerTestProductId()
+    {
+        return [
+            [65, '65'],
+            [4, 4],
+            [5, 5],
+        ];
     }
 }
